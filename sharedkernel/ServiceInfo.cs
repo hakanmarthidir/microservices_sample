734a4069c9e0bb18c2e130098f77caf2c2fb9129
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-09-16 18:34:43 +0200

    consul implementation

---
+++ sharedkernel/ServiceInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace sharedkernel
+{
+    public class ConsulServiceInfo
+    {
+        public string ServiceIP { get; set; }
+        public int ServicePort { get; set; }
+        public string ServiceName { get; set; }
+        public string ConsulHost { get; set; }
+        public int ConsulPort { get; set; }
+    }
+}
+
