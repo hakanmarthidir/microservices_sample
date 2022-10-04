ad0e68dd8e1d70cd64dfc6d9c1062b672c34c0a7
Hakan Hidir <hakan.hidir@technicsatelier.com>
2022-10-04 14:13:37 +0200

    shelve aggregate

---
+++ shelveservice/Domain/ShelveAggregate/Review.cs
@@ -0,0 +1,14 @@
+﻿using sharedkernel;
+
+namespace shelveservice.Domain.ShelveAggregate
+{
+    public class Review : BaseEntity<Guid>
+    {
+        public Guid BookId { get; private set; }
+        public DateTime? DateRead { get; private set; }
+        public byte Rating { get; private set; }
+        public string Comment { get; private set; }
+    }
+
+}
+
